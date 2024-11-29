b60e2175e89f95c529a4d3180e4f1c3e179c0e21
Dan Taft <160355227+DTaft64@users.noreply.github.com>
2024-11-29 14:58:28 -0500

    Update APPS_SCRIPT_code.js

--- APPS_SCRIPT_code.js
+++ APPS_SCRIPT_code.js
@@ -2,7 +2,7 @@ function notOnOpen() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();     // Active spreadsheet
   var sheet = spreadsheet.getSheetByName("Stocks & Flows");    // Sheet named "Stocks & Flows"
 
-  if (!sheet) {
+  if (!sheet) {                                                // SET SHEET NAME HERE
     throw new Error("Sheet 'Stocks & Flows' not found.");      // Error if sheet doesn't exist
   }
   var lastRow = sheet.getLastRow();                            // Last row with data
@@ -10,7 +10,7 @@ function notOnOpen() {
 
   for (var i = 0; i < lastRow; i++) {                          // Loop column G
     if (columnG[i][0] === true) {                              // Check if value is true
-      var targetRow = Math.min(lastRow, i + 21);               // Target row to center true row near 21
+      var targetRow = Math.min(lastRow, i + 23);               // Target row appers at the bottom of the screen, so +23 rows to move to the top of the screen
       sheet.setActiveRange(sheet.getRange("G" + targetRow));   // Set active cell to target row in column G
       break;                                                   // Stop after first true
     }
